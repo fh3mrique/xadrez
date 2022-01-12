a6b3f1fcdcf967133cdb59e81f7ee603d72ca079
fh3mrique <fhemrique55@gmail.com>
2022-01-12 18:22:05 -0300

    primeira classe: posicao

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,5 +1,15 @@
 package aplicacao;
 
+import aplicacao.tabuleirogame.posicao;
+
 public class programa {
     
+    public static void main(String[] args) {
+        
+    posicao p = new posicao(2, 4);
+
+    System.out.println(p);
+
+
+    }
 }
